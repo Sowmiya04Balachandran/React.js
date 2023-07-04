b1fe7e02efedc93274da35679c1f64bcfc3bc673
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-07-04 17:12:12 +0530

    multiple expense item

--- src/Compontents/ExpenseItem.js
+++ src/Compontents/ExpenseItem.js
@@ -1,32 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate=new Date(2023,2,28);
-    const expenseTitle='Car Insurance';
-    const expenseAmount=294.89;
-    const locationOfExpenditure='garge';
+function ExpenseItem(props) {
+    
     return (
       //<h2>Expense Item!</h2>
       <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
-          <h2 >{expenseTitle}</h2>
-          <div className='expense-item__carprice'>${expenseAmount}</div></div>
-          <br></br>
-          <div className='expense-item__location'>{locationOfExpenditure}</div>
-          {/* <div>May 28th 2023</div>
-          <div className='expense-item__description'>
-          <h2>Food</h2>
-          <div className='expense-item__foodprice'>Rs 10</div></div>
-          <div>May 28th 2023</div>
-          <div className='expense-item__description'>
-          <h2>Petrol</h2>
-          <div className='expense-item__petrolprice'>Rs 100</div></div>
-          <div>May 28th 2023</div>
-          <div className='expense-item__description'>
-          <h2>Movies</h2>
-          <div className='expense-item__movieprice'>Rs200</div>
-          </div>  */}
+          <h2 >{props.title}</h2>
+          <div className='expense-item__carprice'>${props.amount}</div></div>
+          
+          <div className='expense-item__location'>{props.locationOfExpenditure}</div>
           
       </div>
     );

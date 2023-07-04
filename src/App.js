77fd3161a091fab71bb5c7e6ef2d248234b06318
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-07-04 13:52:41 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+ import ExpenseItem from './Compontents/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  <div>
+    <h2>Let's Get Started!</h2>
+    <ExpenseItem></ExpenseItem>
+  </div>
   );
 }
 
---
+++ src/Compontents/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+    return (
+      //<h2>Expense Item!</h2>
+      <div>
+        <div>March 28th 2023</div>
+        <div>
+          <h2>Car insurance</h2>
+          <div>$294.67</div>
+          <h2>Food</h2>
+          <div>Rs 10</div>
+          <h2>Petrol</h2>
+          <div>Rs 100</div>
+          <h2>Movies</h2>
+          <div>Rs200</div>
+        </div>
+      </div>
+    );
+  }
+  export default ExpenseItem;
+  

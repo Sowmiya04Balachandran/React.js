026b35859358f73f5184dd25a503eb633a3dfcdc
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-07-04 14:35:40 +0530

    location

--- src/Compontents/ExpenseItem.js
+++ src/Compontents/ExpenseItem.js
@@ -1,18 +1,33 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate=new Date(2023,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=294.89;
+    const locationOfExpenditure='garge';
     return (
       //<h2>Expense Item!</h2>
-      <div>
-        <div>March 28th 2023</div>
-        <div>
-          <h2>Car insurance</h2>
-          <div>$294.67</div>
+      <div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+          <h2 >{expenseTitle}</h2>
+          <div className='expense-item__carprice'>${expenseAmount}</div></div>
+          <br></br>
+          <div className='expense-item__location'>{locationOfExpenditure}</div>
+          {/* <div>May 28th 2023</div>
+          <div className='expense-item__description'>
           <h2>Food</h2>
-          <div>Rs 10</div>
+          <div className='expense-item__foodprice'>Rs 10</div></div>
+          <div>May 28th 2023</div>
+          <div className='expense-item__description'>
           <h2>Petrol</h2>
-          <div>Rs 100</div>
+          <div className='expense-item__petrolprice'>Rs 100</div></div>
+          <div>May 28th 2023</div>
+          <div className='expense-item__description'>
           <h2>Movies</h2>
-          <div>Rs200</div>
-        </div>
+          <div className='expense-item__movieprice'>Rs200</div>
+          </div>  */}
+          
       </div>
     );
   }

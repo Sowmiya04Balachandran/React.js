5bae6bc3260bb382cb3e35a3d82e9dc40a2dbfe6
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-07-08 18:17:02 +0530

    NewExpense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
-// import './App.css';
-import ExpenseItem from "./Compontents/ExpenseItem";
+ import './App.css';
+import React from  'react';
+import Expenses from "./Compontents/expenses/Expenses";
+import NewExpenses from './Compontents/NewExpenses/NewExpenses'
 
 function App() {
   const expenses = [
@@ -9,54 +11,37 @@ function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Super Market ",
+      location: "Super Market ",
     },
     {
        id: "e2", 
        title: "New TV",
         amount: 799.49, 
         date: new Date(2021, 2, 12),
-        locationOfExpenditure:'Electronic Shop' ,
+        location:'Electronic Shop' ,
       },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      locationOfExpenditure:'WorkShop' ,
+      location:'WorkShop' ,
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      locationOfExpenditure:'FurnitureShop' ,
-    },
+      location:'FurnitureShop' ,
+    }
   ];
   return (
-   
+   <div>
+    <NewExpenses />
+    <Expenses items={expenses} />
+   </div>
 
-    <div>
-      <h2>Let's Get Started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={ expenses[0].amount}
-        date={expenses[0].date}
-        locationOfExpenditure={expenses[0].locationOfExpenditure}
-      ></ExpenseItem>
-      <ExpenseItem  title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        locationOfExpenditure={expenses[1].locationOfExpenditure}></ExpenseItem>
-      <ExpenseItem  title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        locationOfExpenditure={expenses[2].locationOfExpenditure}></ExpenseItem>
-      <ExpenseItem  title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        locationOfExpenditure={expenses[3].locationOfExpenditure}></ExpenseItem>
-    </div>
+   
   );
 }
 

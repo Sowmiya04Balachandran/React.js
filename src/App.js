87c6bfbde3c681e3d4db463a63a5f9de9bf65cd7
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-07-10 14:06:36 +0530

    using key

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 // import logo from './logo.svg';
  import './App.css';
-import React from  'react';
-import Expenses from "./Compontents/expenses/Expenses";
-import NewExpenses from './Compontents/NewExpenses/NewExpenses'
+import React  ,{ useState } from  'react';
+import Expenses from "./Compontents/Expense/Expenses";
+import NewExpenses from './Compontents/NewExpenses/NewExpenses';
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "Super Market ",
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: "Super Market ",
+  },
+  {
+     id: "e2", 
+     title: "New TV",
+      amount: 799.49, 
+      date: new Date(2021, 2, 12),
+      location:'Electronic Shop' ,
     },
-    {
-       id: "e2", 
-       title: "New TV",
-        amount: 799.49, 
-        date: new Date(2021, 2, 12),
-        location:'Electronic Shop' ,
-      },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location:'WorkShop' ,
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location:'FurnitureShop' ,
-    }
-  ];
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location:'WorkShop' ,
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location:'FurnitureShop' ,
+  }
+];
 
-const addExpenseHandler=expense=>{
-  console.log('In a App.js');
-  console.log(expense)
-  // setExpense((prevExpense)=>{
-  // return[expense,...prevExpense]
-  // })
-}
+const App=()=> {
+ const[expenses,setExpenses]=useState(DUMMY_EXPENSES); 
+
+const addExpenseHandler=(expense)=>{
+ 
+  setExpenses((prevExpenses)=>{
+  return[expense,  ...prevExpenses];
+  });
+};
 
 
   return (
